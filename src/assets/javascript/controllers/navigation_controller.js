bf01cc57d1464a430d9270337338feff694c3241
Pedro Pinto <pedropinto.web@gmail.com>
2023-05-13 19:28:49 +0100

    feat: 🌟 Add navigation

---
+++ src/assets/javascript/controllers/navigation_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from '@hotwired/stimulus'
+
+export default class NavigationController extends Controller {
+  // static targets = []
+
+  connect() { }
+
+  toggle() {
+    this.element.classList.toggle('is-open')
+  }
+}
--- src/assets/javascript/scripts.js
+++ src/assets/javascript/scripts.js
@@ -1,1 +1,7 @@
-// console.log('> hello')
+import { Application } from '@hotwired/stimulus'
+
+import NavigationController from './controllers/navigation_controller'
+
+window.Stimulus = Application.start()
+
+window.Stimulus.register('navigation', NavigationController)
