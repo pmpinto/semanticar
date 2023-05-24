a824fa7147b7ac87453a608d02a3a4d2ac536c72
Pedro Pinto <pedropinto.web@gmail.com>
2023-05-24 12:18:55 +0100

    chore: 🔧 Test API connection to prod

--- src/assets/javascript/scripts.js
+++ src/assets/javascript/scripts.js
@@ -4,8 +4,20 @@ import NavigationController from './controllers/navigation_controller'
 import HeroController from './controllers/hero_controller'
 import AppController from './controllers/app_controller'
 
+import { API } from './api'
+
 window.Stimulus = Application.start()
 
 window.Stimulus.register('navigation', NavigationController)
 window.Stimulus.register('hero', HeroController)
 window.Stimulus.register('app', AppController)
+
+const tryapi = async () => {
+  console.log('> get count')
+  const value = await API.call('/get-subscribers-count')
+  // const valueInc = await API.call('/increment-subscribers-count')
+  console.log('> value', value)
+  // console.log('> valueUbc', valueInc)
+
+}
+tryapi()
