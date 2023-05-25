26cc6dcf920859719a68467aa5453d24a5d06055
Pedro Pinto <pedropinto.web@gmail.com>
2023-05-25 18:18:03 +0100

    feat: 🌟 Add post page

--- src/assets/javascript/controllers/app_controller.js
+++ src/assets/javascript/controllers/app_controller.js
@@ -2,17 +2,11 @@ import { Controller } from '@hotwired/stimulus'
 import { API } from '../api'
 
 export default class AppController extends Controller {
-  static targets = ['navigation', 'footer']
+  static targets = ['navigation', 'footer', 'highlights']
 
   connect() {
-    const observer = new IntersectionObserver(([entry]) => {
-      this.toggleNavAndFooter(entry)
-    }, {
-      rootMargin: '0px',
-      threshold: 0.5,
-    })
-
-    observer.observe(this.footerTarget)
+    this.observeFooter()
+    this.observeHighlights()
 
     this.toggleNavAndFooter = this.toggleNavAndFooter.bind(this)
   }
@@ -25,5 +19,33 @@ export default class AppController extends Controller {
     event.preventDefault()
     await API.call('/increment-subscribers-count')
     window.location = event.target.closest('a').getAttribute('href')
+  }
+
+  observeFooter() {
+    const observer = new IntersectionObserver(([entry]) => {
+      this.toggleNavAndFooter(entry)
+    }, {
+      rootMargin: '0px',
+      threshold: 0.5,
+    })
+
+    observer.observe(this.footerTarget)
+  }
+
+  observeHighlights() {
+    if (!this.highlightsTarget) return
+
+    const observer = new IntersectionObserver(([entry]) => {
+      this.displayHighlights(entry)
+    }, {
+      rootMargin: '0px',
+      threshold: 0.8,
+    })
+
+    observer.observe(this.highlightsTarget)
+  }
+
+  displayHighlights(entry) {
+    this.highlightsTarget.classList.toggle('is-visible', entry.isIntersecting)
   }
 }
