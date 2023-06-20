847b702985f02862b3ae07a0e7e55f2375d0d361
Pedro Pinto <pedropinto.web@gmail.com>
2023-06-20 16:50:23 +0100

    chore: 🔧 Allow analytics only on production

--- src/_data/build_data.js
+++ src/_data/build_data.js
@@ -1,7 +1,9 @@
 module.exports = () => {
+  const now = new Date()
   return {
-    date: new Date(),
-    timestamp: new Date().getTime(),
-    year: new Date().getFullYear()
+    date: now,
+    timestamp: now.getTime(),
+    year: now.getFullYear(),
+    env: process.env.ENV
   }
 }
